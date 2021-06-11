b9972b6ffa93cbc2e8dbb7d0e24ded0248f49f50
SN0RK7 <82673998+SN0RK7@users.noreply.github.com>
2021-06-11 21:10:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuBtn=document.querySelector(".menu-button");
+const menu=document.querySelector(".nav-menu");
+const menuClose=document.querySelector(".menu-close");
+
+
+menuBtn.addEventListener("click",()=>{
+    menu.classList.add('is-active'); 
+    menuClose.classList.add('is-active');
+});
+menuClose.addEventListener("click",()=>{
+    menu.classList.remove('is-active'); 
+    menuClose.classList.remove('is-active');
+});
